777a6725e2d6623cf3fba26350d024472dfaff97
umicar <77049691+umicar@users.noreply.github.com>
2021-07-19 18:36:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function setup() {
+    canvas = createCanvas(600, 450);
+    canvas.center();
+}
+
+function draw(){
+    image(img,0,0,600,450);
+    fill("#30eb13");
+    stroke("#30eb13");
+    noFill();
+    rect(30,60,400,300)
+}
